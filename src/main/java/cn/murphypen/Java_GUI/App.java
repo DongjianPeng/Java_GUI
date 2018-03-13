fb0231e2e946bfea448d6ce5d9e60b7f711aea93
dongjianpeng <541874767@qq.com>
2018-03-13 22:07:20 +0800

    init

---
+++ src/main/java/cn/murphypen/Java_GUI/App.java
@@ -0,0 +1,13 @@
+package cn.murphypen.Java_GUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/cn/murphypen/Java_GUI/Day01/Day01.java
@@ -0,0 +1,7 @@
+package cn.murphypen.Java_GUI.Day01;
+
+public class Day01 {
+	public static void main(String[] args) {
+		
+	}
+}
