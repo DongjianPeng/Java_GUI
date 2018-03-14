6515a4d8c9c070391a9e29d5f07f30a63dd0b305
dongjianpeng <541874767@qq.com>
2018-03-14 18:45:09 +0800

    rm copyright info

--- src/main/java/cn/murphypen/Java_GUI/Day01/Script.java
+++ src/main/java/cn/murphypen/Java_GUI/Day01/Script.java
@@ -1,21 +1,3 @@
-/**
- * STARPOST CONFIDENTIAL
- * _____________________
- * 
- * [2014] - [2018] StarPost Supply Chain Management Co. (Shenzhen) Ltd. 
- * All Rights Reserved.
- * 
- * NOTICE: All information contained herein is, and remains the property of
- * StarPost Supply Chain Management Co. (Shenzhen) Ltd. and its suppliers, if
- * any. The intellectual and technical concepts contained herein are proprietary
- * to StarPost Supply Chain Management Co. (Shenzhen) Ltd. and its suppliers and
- * may be covered by China and Foreign Patents, patents in process, and are
- * protected by trade secret or copyright law. Dissemination of this information
- * or reproduction of this material is strictly forbidden unless prior written
- * permission is obtained from StarPost Supply Chain Management Co. (Shenzhen)
- * Ltd.
- *
- */
 package cn.murphypen.Java_GUI.Day01;
 
 import java.io.BufferedReader;
