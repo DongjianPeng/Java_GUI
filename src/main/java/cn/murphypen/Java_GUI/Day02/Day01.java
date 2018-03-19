c0de6ee1ae5db8180fc07dbe6ead7eaf0ae8bda1
dongjianpeng <541874767@qq.com>
2018-03-19 18:38:35 +0800

    update

---
+++ src/main/java/cn/murphypen/Java_GUI/Day02/Day01.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018.  MurphyPen
+ */
+
+package cn.murphypen.Java_GUI.Day02;
+
+import com.google.common.collect.Lists;
+
+public class Day01 {
+    public static void main(String[] args) {
+        Double d = new Double(2);
+
+
+    }
+}
