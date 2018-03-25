778844667bf33addca3a321a91ae7fd1d4cb1f28
dongjianpeng <pengdongjian@gmail.com>
2018-03-25 23:29:48 +0800

    backgroud color

---
+++ src/main/java/cn/murphypen/Java_GUI/Day08/MyEvent.java
@@ -0,0 +1,16 @@
+package cn.murphypen.Java_GUI.Day08;
+
+import cn.murphypen.Java_GUI.util.WindowGenerate;
+
+import javax.swing.*;
+
+class MyFrame extends JFrame{
+
+
+}
+public class MyEvent {
+    public static void main(String[] args) {
+        WindowGenerate.run(new MyFrame());
+    }
+
+}
