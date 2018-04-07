1185524e48ddb66cc5680915a800c42878748590
dongjianpeng <pengdongjian@gmail.com>
2018-04-07 19:10:56 +0800

    xx

---
+++ src/main/java/cn/murphypen/Java_GUI/Unit10/MyDb.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018. Murphy
+ */
+
+package cn.murphypen.Java_GUI.Unit10;
+
+public class MyDb {
+    public static void main(String[] args) {
+//        try {
+////            Class.forName("com.mysql.cj.jdbc.Driver");
+////            Connection connection = DriverManager.getConnection("", "", "");
+////            PreparedStatement ps = connection.prepareStatement("");
+////            ps.executeQuery()
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
--- src/main/java/cn/murphypen/Java_GUI/Unit9/MyIO.java
+++ src/main/java/cn/murphypen/Java_GUI/Unit9/MyIO.java
@@ -4,5 +4,14 @@
 
 package cn.murphypen.Java_GUI.Unit9;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 public class MyIO {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        OutputStream os  = new FileOutputStream("dd");
+
+    }
 }
