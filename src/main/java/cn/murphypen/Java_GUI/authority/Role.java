c7861e3cd069b7b710adf38fcaa6a5c43d4159f6
dongjianpeng <pengdongjian@gmail.com>
2018-04-11 23:50:23 +0800

    authority

---
+++ src/main/java/cn/murphypen/Java_GUI/authority/Role.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018. Murphy
+ */
+
+package cn.murphypen.Java_GUI.authority;
+
+public class Role {
+    String id;
+    String appId;
+    String name;
+    String discription;
+    String parentId;
+    String resourceAuth;
+    String oprationAuth;
+}
---
+++ src/main/java/cn/murphypen/Java_GUI/authority/User.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2018. Murphy
+ */
+
+package cn.murphypen.Java_GUI.authority;
+
+public class User {
+    String id;
+
+}
